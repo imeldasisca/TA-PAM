feadab65b84fd09d08a5ac8a905d28801a87aac5
salsabilaaaaaaaaaaaaaaa <salsabilatjahya@student.ub.ac.id>
2022-05-25 15:24:37 +0700

    login and register

---
+++ app/src/main/java/com/example/ta_pam/login_activity.java
@@ -0,0 +1,43 @@
+package com.example.ta_pam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class login_activity extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText usrnm,pass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btnLogin=findViewById(R.id.btn_login);
+        usrnm=findViewById(R.id.et_username);
+        pass=findViewById(R.id.et_password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usrnm.getText().toString();
+                String password = pass.getText().toString();
+                if (username.equals("salsabila") && password.equals("123")) {
+                    Toast.makeText(login_activity.this, "Login Berhasil", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    Toast.makeText(login_activity.this, "Login Gagal", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
